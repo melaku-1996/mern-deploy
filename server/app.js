566bd5e6ce603c327d0ad2ca4312a494c728a55b
mele-bele <kiru12102014ec@gmail.com>
2024-01-07 05:54:32 +0300

    added frontend origin app.js server

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 // middleware
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: "https://mern-deploy-frontend-is6h.onrender.com"
 }))
 
 
